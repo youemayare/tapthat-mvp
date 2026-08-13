54a8a8bc2b12f166a84df4c4c9bf7c2207a77f9e
youemayare <youemayare@gmail.com>
2026-08-13 07:46:17 +0400

    style: remove bouncing scroll indicator from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,12 +65,7 @@ export default function LandingPage() {
           </div>
         </div>
 
-        {/* Scroll indicator */}
-        <div className="absolute bottom-10 left-1/2 -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 rounded-full border-2 border-white/20 flex justify-center pt-2">
-            <div className="w-1 h-2 bg-primary/40 rounded-full" />
-          </div>
-        </div>
+
       </section>
 
       {/* ── Features ── */}
