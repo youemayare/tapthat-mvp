33efe8841dd6922e4e38293f323fc48405261767
youemayare <youemayare@gmail.com>
2026-09-25 16:27:48 +0400

    refactor: completely remove waitlist / founding circle feature

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -146,13 +146,3 @@ export const exchangeIpRecipientRatelimit = hasRedis
   ? makeRatelimit(Ratelimit.slidingWindow(3, '3600 s'), 'tayz:exch_ip_recip')
   : failClosed;
 
-
-/** Waitlist Join */
-export const waitlistJoinRatelimit = hasRedis
-  ? makeRatelimit(Ratelimit.slidingWindow(10, '15 m'), 'tayz:waitlist_join')
-  : failClosed;
-
-/** Waitlist Survey Update */
-export const waitlistSurveyRatelimit = hasRedis
-  ? makeRatelimit(Ratelimit.slidingWindow(20, '15 m'), 'tayz:waitlist_survey')
-  : failClosed;
